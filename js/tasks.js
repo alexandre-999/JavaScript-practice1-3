0d265d94096d528ff88f1ad022fc35962c832a69
Alexandre Oyama <94652462+alexandre-999@users.noreply.github.com>
2021-12-22 18:27:36 +0900

    ）後の；前の空白削除

--- js/tasks.js
+++ js/tasks.js
@@ -1,25 +1,25 @@
-'use strict';
+'use strict' ;
 
 const title = '============================\n現在持っているタスクの一覧\n============================'
 
 let tasks = [ '買い物' , '掃除' , '散歩' ] ;
 
-console.log( title ) ;
+console.log( title );
 tasks.forEach( function( task ) {
-  console.log( tasks.indexOf( task ) , ':' , task ) ;
-}) ;
+  console.log( tasks.indexOf( task ) , ':' , task );
+});
 
-let input = prompt( 'タスクを入力してください。' ) ;
+let input = prompt( 'タスクを入力してください。' );
 while( input !== null ) {
   if( input === '' ) {
-  alert( '何も入力されていません！' ) ;
-  input = prompt( 'もう一度タスクを入力してください' ) ;
+  alert( '何も入力されていません！' );
+  input = prompt( 'もう一度タスクを入力してください' );
 } else if ( input !== '' ) {
-  tasks.push( input ) ;
+  tasks.push( input );
   console.log( title );
   tasks.forEach( function( task ) {
-  console.log( tasks.indexOf( task ) , ':' , task ) ;
-  }) ;
-  alert( '新しいタスクを追加しました。' ) ;
-  input = prompt( 'タスクを入力してください。' ) ;
+  console.log( tasks.indexOf( task ) , ':' , task );
+  });
+  alert( '新しいタスクを追加しました。' );
+  input = prompt( 'タスクを入力してください。' );
 }} ;
