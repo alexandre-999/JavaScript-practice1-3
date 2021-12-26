d1532d627d3dd2f4252f6cd5a3d750874e0f8593
Alexandre Oyama <94652462+alexandre-999@users.noreply.github.com>
2021-12-26 15:58:51 +0900

    コード修正完了（処理一回に変更）

--- js/tasks.js
+++ js/tasks.js
@@ -1,25 +1,22 @@
 'use strict';
 
-const title = '============================\n現在持っているタスクの一覧\n============================'
+const title = '============================\n現在持っているタスクの一覧\n============================';
 
-let tasks = ['買い物' , '掃除' , '散歩'];
+const tasks = ['買い物' , '掃除' , '散歩'];
 
-console.log(title);
-tasks.forEach(function(task) {
-  console.log(tasks.indexOf(task) , ':' , task);
+tasks.forEach((task, index) => {
+  console.log(`${index} : ${task}`);
 });
 
 let input = prompt('タスクを入力してください。');
-while(input !== null) {
-  if(input === '') {
+if(input === '') {
   alert('何も入力されていません！');
-  input = prompt('もう一度タスクを入力してください');
-} else if(input !== '') {
+} else if((input !== '') && (input !== null)) {
   tasks.push(input);
   console.log(title);
-  tasks.forEach(function(task) {
-    console.log(tasks.indexOf(task) , ':' , task);
-  });
+  tasks.forEach((task, index) => {
+    console.log(`${index} : ${task}`);
+});
   alert('新しいタスクを追加しました。');
   input = prompt('タスクを入力してください。');
-}};
+};
